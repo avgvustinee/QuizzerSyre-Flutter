21c61c60334f7ee89e963eef411fe9153ff37f60
codewithsyre <augustineshoks7@gmail.com>
2022-12-16 23:36:20 +0200

    initial commit

---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+// class
+class Question {
+  // Data Fields
+  String questionText;
+  bool questionAnswer;
+  // Constructor
+  Question({String q, bool a}) {
+    questionText = q;
+    questionAnswer = a;
+  }
+}
